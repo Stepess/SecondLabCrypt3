f42fa32ecff4ca786b6db3b9fd636ad3a214062f
Stepess <stepanersh@gmail.com>
2019-02-23 21:20:48 +0200

    cipher implementation - not tested

--- src/main/java/ua/crypto/App.java
+++ src/main/java/ua/crypto/App.java
@@ -1,7 +1,13 @@
 package ua.crypto;
 
+import ua.crypto.cipher.Misty;
+
+import java.util.Arrays;
+
 public class App {
+
     public static void main(String[] args) {
 
+        Misty misty = new Misty();
     }
 }
--- src/main/java/ua/crypto/util/ShiftUtils.java
+++ src/main/java/ua/crypto/util/ShiftUtils.java
@@ -2,6 +2,22 @@ package ua.crypto.util;
 
 public class ShiftUtils {
 
+    public static long rightCyclicShift(long i, int step) {
+        return Long.rotateRight(i, step);
+    }
+
+    public static long leftCyclicShift(long i, int step) {
+        return Long.rotateLeft(i, step);
+    }
+
+    public static long rightShift(long i, int step) {
+        return i >>> step;
+    }
+
+    public static long leftShift(long i, int step) {
+        return i << step;
+    }
+
     public static int rightCyclicShift(int i, int step) {
         return Integer.rotateRight(i, step);
     }
