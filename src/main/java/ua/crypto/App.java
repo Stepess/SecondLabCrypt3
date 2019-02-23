410b18203ac26a6e21d5efa1f1ca2204a2bfa712
Stepess <stepanersh@gmail.com>
2019-02-23 22:56:40 +0200

    writen but didn't work

--- src/main/java/ua/crypto/App.java
+++ src/main/java/ua/crypto/App.java
@@ -1,8 +1,20 @@
 package ua.crypto;
 
+import ua.crypto.DigitalSignature.Pair;
+import ua.crypto.DigitalSignature.Signer;
+
+import java.math.BigInteger;
+
 public class App {
 
     public static void main(String[] args) {
+        Signer signer =  new Signer();
 
+        Pair<BigInteger, BigInteger> key = signer.generateKeys();
+
+        long hash = 2323523523423L;
+        Pair<BigInteger, BigInteger> sign = signer.sign(hash, key.getLeft());
+
+        System.out.println(signer.verify(sign.getRight(), key.getRight(), sign.getLeft(), hash));
     }
 }
