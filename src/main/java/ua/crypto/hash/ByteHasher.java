ab60bfc2e46ce9c04b95aad9de2719849ee3b086
Stepess <stepanersh@gmail.com>
2019-03-12 00:04:22 +0200

    refactor

--- src/main/java/ua/crypto/hash/ByteHasher.java
+++ src/main/java/ua/crypto/hash/ByteHasher.java
@@ -3,15 +3,10 @@ package ua.crypto.hash;
 import ua.crypto.cipher.Misty;
 import ua.crypto.util.ShiftUtils;
 
-import java.util.Arrays;
-
 public class ByteHasher {
 
-    public static final int BLOCK_LENGTH_IN_BITS = 64;
     public static final int BLOCK_LENGTH_IN_BYTES = 8;
     public static final int BITS_IN_BYTE = 8;
-    public static final byte PADDING_SPLITTER = 1;
-    public static final byte PADDING_FILLER = 0;
 
     private Misty misty = new Misty();
 
@@ -29,33 +24,9 @@ public class ByteHasher {
         return misty.encrypt(hash, text ^ hash) ^ text ^ hash;
     }
 
-    /*private int getLengthWithPadding(int initialLength) {
-        int missing = ++initialLength % (BLOCK_LENGTH_IN_BITS);
-        return missing == 0 ? initialLength : initialLength + BLOCK_LENGTH_IN_BITS - missing;
-    }*/
-
     private int getLengthWithPadding(int initialLength) {
         int missing = ++initialLength % (BLOCK_LENGTH_IN_BYTES);
         return missing == 0 ? initialLength / BLOCK_LENGTH_IN_BYTES : (initialLength + BLOCK_LENGTH_IN_BYTES - missing) / BLOCK_LENGTH_IN_BYTES;
-    }
-
-    /*private int getLengthWithPadding(int initialLength) {
-        int missing = ++initialLength % (BLOCK_LENGTH_IN_BITS);
-        return missing == 0 ? initialLength / BLOCK_LENGTH_IN_BITS : (initialLength + BLOCK_LENGTH_IN_BITS - missing) / BLOCK_LENGTH_IN_BITS;
-    }*/
-
-    private long setBitAtTheEnd(long num) {
-        return num + ShiftUtils.leftShift(1, (int)(Math.log(Long.highestOneBit(num))/Math.log(2)+1));
-    }
-
-    private int calculateAppenderBItPosition(int lengthInByte) {
-        int lengthInBits = lengthInByte << 3;
-        System.out.println(lengthInBits >> 6);
-        int num = 0b1000_1111;
-         num ^= 1 << (lengthInBits & 63);
-        System.out.println(Integer.toBinaryString(num));
-        System.out.println(lengthInBits);
-        return 0;
     }
 
     private long[] padding(byte[] text) {
